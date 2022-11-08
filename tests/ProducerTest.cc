3e94653c820bf5a89f04feca04ec8990654d3699
Baodi Shi <wudixiaolongbao@icloud.com>
2022-11-08 23:23:56 +0800

    [feat] Support WaitForExclusive and ExclusiveWithFencing producer access mode.

--- tests/ProducerTest.cc
+++ tests/ProducerTest.cc
@@ -275,7 +275,8 @@ TEST(ProducerTest, testChunkingMaxMessageSize) {
 TEST(ProducerTest, testExclusiveProducer) {
     Client client(serviceUrl);
 
-    std::string topicName = "persistent://public/default/testExclusiveProducer";
+    std::string topicName =
+        "persistent://public/default/testExclusiveProducer" + std::to_string(time(nullptr));
 
     Producer producer1;
     ProducerConfiguration producerConfiguration1;
@@ -294,6 +295,89 @@ TEST(ProducerTest, testExclusiveProducer) {
     ProducerConfiguration producerConfiguration3;
     producerConfiguration3.setProducerName("p-name-3");
     ASSERT_EQ(ResultProducerBusy, client.createProducer(topicName, producerConfiguration3, producer3));
+}
+
+TEST(ProducerTest, testWaitForExclusiveProducer) {
+    Client client(serviceUrl);
+
+    std::string topicName =
+        "persistent://public/default/testWaitForExclusiveProducer" + std::to_string(time(nullptr));
+
+    Producer producer1;
+    ProducerConfiguration producerConfiguration1;
+    producerConfiguration1.setProducerName("p-name-1");
+    producerConfiguration1.setAccessMode(ProducerConfiguration::Exclusive);
+
+    ASSERT_EQ(ResultOk, client.createProducer(topicName, producerConfiguration1, producer1));
+
+    ASSERT_EQ(ResultOk, producer1.send(MessageBuilder().setContent("content").build()));
+
+    Producer producer2;
+    ProducerConfiguration producerConfiguration2;
+    producerConfiguration2.setProducerName("p-name-2");
+    producerConfiguration2.setAccessMode(ProducerConfiguration::WaitForExclusive);
+
+    Latch latch(1);
+    client.createProducerAsync(topicName, producerConfiguration2,
+                               [&latch, &producer2](Result res, Producer producer) {
+                                   ASSERT_EQ(ResultOk, res);
+                                   latch.countdown();
+                                   producer2 = producer;
+                               });
+
+    // when p1 close, p2 success created.
+    producer1.close();
+    latch.wait();
+    ASSERT_EQ(ResultOk, producer2.send(MessageBuilder().setContent("content").build()));
+
+    producer2.close();
+}
+
+TEST(ProducerTest, testExclusiveWithFencingProducer) {
+    Client client(serviceUrl);
+
+    std::string topicName =
+        "persistent://public/default/testExclusiveWithFencingProducer" + std::to_string(time(nullptr));
+
+    Producer producer1;
+    ProducerConfiguration producerConfiguration1;
+    producerConfiguration1.setProducerName("p-name-1");
+    producerConfiguration1.setAccessMode(ProducerConfiguration::Exclusive);
+
+    ASSERT_EQ(ResultOk, client.createProducer(topicName, producerConfiguration1, producer1));
+    producer1.send(MessageBuilder().setContent("content").build());
+
+    Producer producer2;
+    ProducerConfiguration producerConfiguration2;
+    producerConfiguration2.setProducerName("p-name-2");
+    producerConfiguration2.setAccessMode(ProducerConfiguration::WaitForExclusive);
+
+    Latch latch(1);
+    client.createProducerAsync(topicName, producerConfiguration2,
+                               [&latch, &producer2](Result res, Producer producer) {
+                                   // producer 2 fenced
+                                   ASSERT_EQ(ResultProducerFenced, res);
+                                   latch.countdown();
+                                   producer2 = producer;
+                               });
+
+    Producer producer3;
+    ProducerConfiguration producerConfiguration3;
+    producerConfiguration3.setProducerName("p-name-3");
+    producerConfiguration3.setAccessMode(ProducerConfiguration::ExclusiveWithFencing);
+    ASSERT_EQ(ResultOk, client.createProducer(topicName, producerConfiguration3, producer3));
+    ASSERT_EQ(ResultOk, producer3.send(MessageBuilder().setContent("content").build()));
+
+    latch.wait();
+
+    // producer 1 fenced
+    ASSERT_EQ(ResultProducerFenced, producer1.send(MessageBuilder().setContent("content").build()));
+
+    ASSERT_EQ(ResultProducerNotInitialized, producer2.send(MessageBuilder().setContent("content").build()));
+
+    producer1.close();
+    producer2.close();
+    producer3.close();
 }
 
 TEST_P(ProducerTest, testFlushNoBatch) {
